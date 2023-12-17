7fb47ec223ff9a9842f14c058d89b1e2974b8876
Yahor \|/ <yagorpraca@gmail.com>
2023-12-17 22:59:28 +0300

    фикс первого

--- src/Task1.java
+++ src/Task1.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-
-
 
 public class Task1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите строку:");
         String input = scanner.nextLine();
-        String[] words = input.split(" ");
+        String[] words = input.split("\\s+");
 
         for (String word : words) {
-            if (word.length() >= 2 && word.length() <= 6 && Pattern.matches("^[\\p{Lu}]+$", word)) {
-                System.out.println( " Аббревиатура: " + word);
+            if (word.length() >= 2 && word.length() <= 6) {
+                Matcher m = Pattern.compile("[\\p{Lu}]+").matcher(word);
+                if (m.find()) {
+                    System.out.println( " Аббревиатура: " + m.group());
+                }
             }
         }
     }
 }
+
